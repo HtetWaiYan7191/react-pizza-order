e4621a76b5f42434322cbceb61f1c5d6df64af13
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-02-27 15:00:13 +0630

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import Menu from "./features/menu/Menu"
+import CreateOrder from "./features/order/CreateOrder"
+import OrderDetail from "./features/order/OrderDetail"
+import Cart from "./features/cart/Cart"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/cart',
+    element: <Cart/>
+  },
+  {
+    path: '/menu',
+    element: <Menu/>
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder/>
+  },
+  {
+    path: '/order/:orderId',
+    element: <OrderDetail/>
+  }
+])
 export default function App() {
 
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router}/>
 }
