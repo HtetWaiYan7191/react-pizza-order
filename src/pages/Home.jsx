a153c314e4f4357feb457d000cb8bf0d5d9104af
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-02-27 20:58:50 +0630

    Add the loader while fetching the menu data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <p>home</p>
+      <Link to="/menu">Menu</Link>
+    </div>
   )
 }
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
 import CartOverview from '../features/cart/CartOverview'
 import Header from './Header'
-import {Outlet} from 'react-router-dom'
+import {Outlet, useNavigation} from 'react-router-dom'
+import Loader from './Loader';
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading'
   return (
     <section>
         <Header/>
+        {isLoading && <Loader/>}
         <main className="content">
             <h2>content</h2>
             <Outlet/>
