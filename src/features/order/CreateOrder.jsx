322feb41cd47ded33cbfec3d7df518f430704a65
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-03-01 11:29:47 +0630

    Add error handling in form actions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,32 +1,46 @@
-import { Form } from "react-router-dom";
+import { Form, useNavigation, useActionData } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import { redirect } from "react-router-dom";
 
 export async function action({ request }) {
   const formData = await request.formData();
- const data = Object.fromEntries(formData);
- const order = {
-  ...data,
-  cart: JSON.parse(data.cart),
-  priority: data.priority === 'on',
- };
+  const data = Object.fromEntries(formData);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
 
- const newOrder = await createOrder(order);
- return redirect(`/order/${newOrder.id}`)
+  const errors = {};
+  if (!isValidPhoneNumber(data.phone))
+    errors.phone =
+      "Please give us your correct phone number 🤔. We might need to contact you 🤨";
 
+  if (Object.keys(errors).length > 0) return errors;
+
+  const newOrder = await createOrder(order);
+  return redirect(`/order/${newOrder.id}`);
+}
+
+function isValidPhoneNumber(phoneNumber) {
+  // Regular expression to match a valid phone number
+  const phoneRegex =
+    /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+  // Check if the phoneNumber matches the regex pattern
+  return phoneRegex.test(phoneNumber);
 }
 
 const fakeCart = [
   {
     pizzaId: 1,
-    name: 'spinach and mushroom',
+    name: "spinach and mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
   },
   {
     pizzaId: 2,
-    name: 'spinach ',
+    name: "spinach ",
     quantity: 4,
     unitPrice: 15,
     totalPrice: 15,
@@ -34,14 +48,20 @@ const fakeCart = [
 
   {
     pizzaId: 2,
-    name: ' mushroom',
+    name: " mushroom",
     quantity: 6,
     unitPrice: 15,
     totalPrice: 15,
   },
-]
+];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  console.log(navigation.state);
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
+  console.log(formErrors);
+
   return (
     <div className="order-container">
       <h2>Jonas</h2>
@@ -49,17 +69,24 @@ function CreateOrder() {
       <Form method="POST" className="order-new-form">
         <h2 className="text-lg font-semibold">Ready to order ? Lets go !</h2>
         <div className="">
-          <label htmlFor="customer" id="customer">First Name</label>
+          <label htmlFor="customer" id="customer">
+            First Name
+          </label>
           <input required className="border-2" type="text" name="customer" />
         </div>
 
         <div className="">
-          <label htmlFor="phone" id="phone">Phone Number</label>
+          <label htmlFor="phone" id="phone">
+            Phone Number
+          </label>
           <input required className="border-2" type="tel" name="phone" />
+          {formErrors?.phone && <span>{formErrors.phone}</span>}
         </div>
 
         <div className="">
-          <label htmlFor="address" id="address">Address</label>
+          <label htmlFor="address" id="address">
+            Address
+          </label>
           <input required className="border-2" type="text" name="address" />
         </div>
 
@@ -68,7 +95,9 @@ function CreateOrder() {
 
         <div className="btn-container">
           <input type="hidden" name="cart" value={JSON.stringify(fakeCart)} />
-          <button className="px-3 py-1 border">Order now</button>
+          <button disabled={isSubmitting} className="px-3 py-1 border">
+            {isSubmitting ? "Placing Order" : "Order Now"}
+          </button>
         </div>
       </Form>
     </div>
