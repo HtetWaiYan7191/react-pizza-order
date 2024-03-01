93d3a9d1b135d101eca4a2d8ac81f76f7755ed9d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-03-01 14:59:02 +0630

    Style loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,7 @@
 export default function Loader() {
   return (
-    <div>Loading...</div>
+    <div className="fixed top-0 flex items-center justify-center w-full h-screen bg-gray-200 loader-container opacity-30">
+      <span className="w-20 h-20 border-t-4 border-b-4 border-yellow-500 rounded-full animate-spin"></span>
+    </div>
   )
 }
